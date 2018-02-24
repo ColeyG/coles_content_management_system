cd7b0e260f2531ae95ef57a54165f46303d9ee3c
ColeyG <cjgeerts@gmail.com>
2018-02-23 22:22:56 -0500

    fixing dual user issue

--- admin/phpscripts/user.php
+++ admin/phpscripts/user.php
@@ -1,26 +1,34 @@
 <?php
     function createUser($username,$password,$email,$userlvl,$fname){
         include('connect.php');
-        if(empty($username)||empty($password)||empty($email)||empty($fname)){
-            $message = "Fill out all fields!";
+        $checkDB = "SELECT * FROM tbl_users WHERE users_name = '{$username}'";
+        $userCheck = mysqli_query($link,$checkDB);
+        $foundUserCheck=mysqli_fetch_array($userCheck,MYSQLI_ASSOC);
+        if($foundUserCheck['users_id']>0){
+            $message = "Username is taken.";
             return $message;
-        }
-        $enPass = password_hash($password, PASSWORD_DEFAULT);
-        $userString = "INSERT INTO tbl_users VALUES (NULL,'{$username}','$enPass','{$email}',NULL,NULL,'{$userlvl}','{$fname}')";
-        //echo $userString;
-        $userQuery = mysqli_query($link,$userString);
-        if($userQuery){
-            /* THE FOLLOWING CODE IS THE MAILER. This will not work on a non-live site, but does succesfully mail. Tested it! :) (it calls the mailer.php function in functions.php.)*/
-            mailer($email,'Account Verification',"Account Verification for Cole's CMS.com, Here are your credentials:<br> Username: ".$username." Password: ".$password." Register at: colescms.com");
-            redirect_to('admin_index.php');
         }else{
-            $message = "There was a problem setting up this user.";
-            return $message;
+            if(empty($username)||empty($password)||empty($email)||empty($fname)){
+                $message = "Fill out all fields!";
+                return $message;
+            }
+            $enPass = password_hash($password, PASSWORD_DEFAULT);
+            $userString = "INSERT INTO tbl_users VALUES (NULL,'{$username}','$enPass','{$email}',NULL,NULL,'{$userlvl}','{$fname}')";
+            //echo $userString;
+            $userQuery = mysqli_query($link,$userString);
+            if($userQuery){
+                /* THE FOLLOWING CODE IS THE MAILER. This will not work on a non-live site, but does succesfully mail. Tested it! :) (it calls the mailer.php function in functions.php.)*/
+                mailer($email,'Account Verification',"Account Verification for Cole's CMS.com, Here are your credentials:<br> Username: ".$username." Password: ".$password." Register at: colescms.com");
+                redirect_to('admin_index.php');
+            }else{
+                $message = 'There was an issue setting up this account.';
+                return $message;
+            }
         }
-        
 
         mysqli_close($link);
     }
+    
 
     function editUser($id,$editPassCheck,$username,$email,$fname,$newPass){
         include('connect.php');
